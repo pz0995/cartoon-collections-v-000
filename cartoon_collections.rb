6de9720db612f59dc6663c51f1cd471c33b1160c
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-19 23:29:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,10 @@
 def roll_call_dwarves# code an argument here
-dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+    dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 puts "#{dwarves}"
 end
 
 def summon_captain_planet# code an argument here
-  # Your code here
+    planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 end
 
 def long_planeteer_calls# code an argument here
