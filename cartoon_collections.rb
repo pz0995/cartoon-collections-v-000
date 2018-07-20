1bdb1253a46550fda1d51f801bc75e7c117253ad
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-20 02:17:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,6 +5,8 @@ end
 
 def summon_captain_planet(planeteer_calls)
     planeteer_calls = ["earth", "wind", "fire", "water", "heart"].titleize
+planeteer_calls =fruits.map do |calls|
+calls.upcase
 end
 
 def long_planeteer_calls# code an argument here
