7d9a53b5e0053141afe58b0ad1831021e03e60ec
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-20 02:37:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,6 @@
 def roll_call_dwarves# code an argument here
     dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
-    count = 0
-    dwarves {|dwarves| count += dwarf.length}
-    puts "#{dwarves < 4 }"
+
 end
 
 def summon_captain_planet?(planeteer_calls)
@@ -14,6 +12,10 @@ end
 def long_planeteer_calls# code an argument here
   short_words = ["puff", "go", "two"]
   assorted_words = ["two", "go", "industrious", "bop"]
+  count = 0
+  short_words = {|short_words| count += short.length}
+  assorted_words = {|assorted_words| count += asst.length}
+  puts "#{short > 4 && asst > 4 }"
 end
 
 def find_the_cheese# code an argument here
